23c75af649297cec7f4efc70455a1d4d8fe065af
izisfro <izis.froyhov@nav.no>
2021-09-13 15:38:01 +0200

    utvider tiltakDTO
    Co-authored-by: Alexander Gård <alexander.gard@nav.no>
    Co-authored-by: Magnus Evensberget <magnus.evensberget@nav.no>
    Co-authored-by: Einar Bjerve <einar.bjerve@nav.no>

--- tiltak/src/main/kotlin/no/nav/amt/tiltak/tiltak/controllers/TiltakController.kt
+++ tiltak/src/main/kotlin/no/nav/amt/tiltak/tiltak/controllers/TiltakController.kt
@@ -1,15 +1,15 @@
 package no.nav.amt.tiltak.tiltak.controllers
 
-import no.nav.amt.tiltak.tiltak.controllers.dto.TiltakInstansDTO
+import no.nav.amt.tiltak.tiltak.controllers.dto.TiltakDTO
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/api/tiltak")
 class TiltakController {
 
-	@GetMapping("/{tiltakId}/instanser")
-	fun hentInstanser(@PathVariable tiltakId: String): List<TiltakInstansDTO> {
+	@GetMapping
+	fun hentTiltak(@RequestParam tiltaksleverandorId: String): List<TiltakDTO> {
 		TODO()
 	}
 
--- tiltaksleverandor/src/main/kotlin/no/nav/amt/tiltak/tiltaksleverandor/controllers/dto/TiltakDTO.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.amt.tiltak.tiltaksleverandor.controllers.dto
-
-import java.util.*
-
-data class TiltakDTO(
-	val id: UUID
-)
