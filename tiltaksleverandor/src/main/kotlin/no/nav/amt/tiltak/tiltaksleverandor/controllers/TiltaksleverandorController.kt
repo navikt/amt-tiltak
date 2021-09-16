fa11baa8fcd3e833dbd88fe785542b5bd55874fb
Einar Bjerve <einarbjerve@Einars-MacBook-Pro-2.local>
2021-09-16 14:49:57 +0200

    Returner mockdata på ansatte for å muligjøre test
    Co-authored-by: Izis Frøyhov <izis.froyhov@nav.no>
    Co-authored-by: Alexander Gård <alexander.gard@nav.no>

--- tiltaksleverandor/src/main/kotlin/no/nav/amt/tiltak/tiltaksleverandor/controllers/TiltaksleverandorController.kt
+++ tiltaksleverandor/src/main/kotlin/no/nav/amt/tiltak/tiltaksleverandor/controllers/TiltaksleverandorController.kt
@@ -1,16 +1,10 @@
 package no.nav.amt.tiltak.tiltaksleverandor.controllers
 
-import no.nav.amt.tiltak.tiltaksleverandor.controllers.dto.TiltakDTO
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/api/tiltaksleverandor")
 class TiltaksleverandorController {
 
-	@GetMapping("/{tiltaksleverandorId}/tiltak")
-	fun hentTiltak(@PathVariable tiltaksleverandorId: String): List<TiltakDTO> {
-		TODO()
-	}
+	// TODO unødvendig inntil videre?
 
 }
