b7d85d9d5f7529fccdd1cd4b43d8eea0bb87bdad
Einar Bjerve <einarbjerve@Einars-MacBook-Pro-2.local>
2021-09-16 14:49:57 +0200

    Returner mockdata på ansatte for å muligjøre test
    Co-authored-by: Izis Frøyhov <izis.froyhov@nav.no>
    Co-authored-by: Alexander Gård <alexander.gard@nav.no>

--- tiltaksleverandor/src/main/kotlin/no/nav/amt/tiltak/tiltaksleverandor/controllers/AnsattController.kt
+++ tiltaksleverandor/src/main/kotlin/no/nav/amt/tiltak/tiltaksleverandor/controllers/AnsattController.kt
@@ -2,17 +2,29 @@ package no.nav.amt.tiltak.tiltaksleverandor.controllers
 
 import no.nav.amt.tiltak.core.port.Tiltaksleverandor
 import no.nav.amt.tiltak.tiltaksleverandor.controllers.dto.AnsattDTO
+import no.nav.amt.tiltak.tiltaksleverandor.controllers.dto.AnsattRolle
+import no.nav.amt.tiltak.tiltaksleverandor.controllers.dto.VirksomhetDTO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController("/api/tiltaksleverandor/ansatt")
 class AnsattController(
     private val service: Tiltaksleverandor
 ) {
 
-    @GetMapping("/me")
+    @GetMapping("/meg")
     fun getInnloggetAnsatt(): AnsattDTO {
-        TODO()
+        return AnsattDTO(
+			id = UUID.randomUUID(),
+			fornavn = "Navn",
+			etternavn = "Navnsen",
+			virksomheter = listOf(VirksomhetDTO(
+				id = UUID.randomUUID(),
+				virksomhetsnummer = "123456789",
+				virksomhetsnavn = "Mulgiheter",
+				roller = listOf(AnsattRolle.KOORDINATOR, AnsattRolle.VEILEDER)
+			))) // TODO dummy data
     }
 
 }
