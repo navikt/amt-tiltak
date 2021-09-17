efccf3910a1e1e54ba02511ed50940093e2a5361
Einar Bjerve <einar.bjerve@nav.no>
2021-09-17 11:40:22 +0200

    Returnerer testdata
    Co-authored-by: Alexander Gård <alexander.gard@nav.no>

--- tiltak/src/main/kotlin/no/nav/amt/tiltak/tiltak/controllers/TiltakController.kt
+++ tiltak/src/main/kotlin/no/nav/amt/tiltak/tiltak/controllers/TiltakController.kt
@@ -1,16 +1,61 @@
 package no.nav.amt.tiltak.tiltak.controllers
 
+import no.nav.amt.tiltak.core.domain.tiltak.TiltakInstans
+import no.nav.amt.tiltak.tiltak.controllers.dto.TiltakDto
 import no.nav.amt.tiltak.tiltak.controllers.dto.TiltakInstansDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.util.*
 
 @RestController("/api/tiltak")
 class TiltakController {
 
 	@GetMapping
 	fun hentAlleTiltak(@RequestParam("tiltaksleverandorId") tiltaksleverandorId: String): List<TiltakInstansDto> {
-		TODO()
+		return listOf(
+			TiltakInstansDto(
+				id = UUID.randomUUID(),
+				navn = "Truckførerkurs",
+				startdato = ZonedDateTime.now().plusDays(1),
+				sluttdato = ZonedDateTime.now().plusDays(3),
+				status = TiltakInstans.Status.IKKE_STARTET,
+				antallDeltagere = 8,
+				deltagerKapasitet = 4,
+				TiltakDto(
+					tiltakskode = "GRUPPEAMO",
+					tiltaksnavn = "Gruppe AMO"
+				),
+			),
+			TiltakInstansDto(
+				id = UUID.randomUUID(),
+				navn = "Sveisekurs",
+				startdato = ZonedDateTime.now().minusDays(1),
+				sluttdato = ZonedDateTime.now().plusDays(8),
+				status = TiltakInstans.Status.GJENNOMFORES,
+				antallDeltagere = 5,
+				deltagerKapasitet = 8,
+				TiltakDto(
+					tiltakskode = "GRUPPEAMO",
+					tiltaksnavn = "Gruppe AMO"
+				),
+			),
+			TiltakInstansDto(
+				id = UUID.randomUUID(),
+				navn = "Jobbklubb",
+				startdato = ZonedDateTime.now().minusDays(10),
+				sluttdato = ZonedDateTime.now().minusDays(8),
+				status = TiltakInstans.Status.AVSLUTTET,
+				antallDeltagere = 5,
+				deltagerKapasitet = 8,
+				TiltakDto(
+					tiltakskode = "JOBBK",
+					tiltaksnavn = "Jobbklubb"
+				),
+			)
+		)
 	}
 
 }
--- tiltak/src/main/kotlin/no/nav/amt/tiltak/tiltak/controllers/TiltakInstansController.kt
+++ tiltak/src/main/kotlin/no/nav/amt/tiltak/tiltak/controllers/TiltakInstansController.kt
@@ -1,16 +1,41 @@
 package no.nav.amt.tiltak.tiltak.controllers
 
+import no.nav.amt.tiltak.core.domain.tiltak.Deltager
 import no.nav.amt.tiltak.tiltak.controllers.dto.TiltakDeltagerDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.util.*
 
 @RestController("/api/tiltak-instans")
 class TiltakInstansController {
 
 	@GetMapping("/{tiltkInstansId}/deltagere")
 	fun hentDeltagere(@PathVariable("tiltkInstansId") tiltkInstansId: String): List<TiltakDeltagerDto> {
-		TODO()
+		return listOf(
+			TiltakDeltagerDto(
+				id = UUID.randomUUID(),
+				fornavn = "Stødig",
+				mellomnavn = "Mektig",
+				etternavn = "Bord",
+				fodselsdato = LocalDate.of(2001, 6, 8),
+				startdato = ZonedDateTime.now().minusDays(1),
+				sluttdato = ZonedDateTime.now().plusDays(1),
+				status = Deltager.Status.GJENNOMFORES
+			),
+			TiltakDeltagerDto(
+				id = UUID.randomUUID(),
+				fornavn = "Prektig",
+				mellomnavn = "",
+				etternavn = "Telefon",
+				fodselsdato = LocalDate.of(2001, 6, 8),
+				startdato = ZonedDateTime.now().plusDays(1),
+				sluttdato = ZonedDateTime.now().plusDays(4),
+				status = Deltager.Status.GJENNOMFORES
+			),
+		)
 	}
 
 }
