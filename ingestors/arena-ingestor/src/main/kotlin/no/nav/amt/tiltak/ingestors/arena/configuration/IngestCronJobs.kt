6c0d35a08d5f34e9fff9fe6da19f1a55c26c7496
Magnus Evensberget <magnus.evensberget@nav.no>
2021-10-07 14:22:19 +0200

    La til en schedule locker for å kjøre på flere noder

--- ingestors/arena-ingestor/src/main/kotlin/no/nav/amt/tiltak/ingestors/arena/configuration/IngestCronJobs.kt
+++ ingestors/arena-ingestor/src/main/kotlin/no/nav/amt/tiltak/ingestors/arena/configuration/IngestCronJobs.kt
@@ -1,25 +1,51 @@
 package no.nav.amt.tiltak.ingestors.arena.configuration
 
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import no.nav.amt.tiltak.ingestors.arena.ArenaDataProcessor
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import javax.sql.DataSource
 
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT60M")
 open class IngestCronJobs(
     private val processor: ArenaDataProcessor
 ) {
 
-    // TODO Schedule locked (https://www.baeldung.com/shedlock-spring)
-
-    // TODO Every (minute?)
-    fun processUningestedArenaData() {
-        processor.processUningestedMessages()
+    companion object {
+        private val logger = LoggerFactory.getLogger(IngestCronJobs::class.java)
     }
 
-    // TODO Run midnight
-    fun processAllUningestedArenaData() {
-        processor.processFailedMessages()
+    @Bean
+    open fun lockProvider(datasource: DataSource): LockProvider {
+        return JdbcTemplateLockProvider(datasource)
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    @SchedulerLock(
+        name = "arena-ingest",
+        lockAtMostFor = "PT3M"
+    )
+    fun processUningestedArenaData() {
+        logger.debug("Starting processing job for uningested Arena Data")
+//        processor.processUningestedMessages()
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    @SchedulerLock(
+        name = "arena-ingest-failed-retry",
+        lockAtMostFor = "PT60M"
+    )
+    fun processFailedArenaData() {
+        logger.debug("Starting processing job for failed Arena Data")
+//        processor.processFailedMessages()
     }
 
 }
