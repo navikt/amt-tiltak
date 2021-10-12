6dc94a6dead54f753e7bb59312e89d596e837af3
Alexander Gård <alex.gaard@hotmail.com>
2021-10-12 14:39:49 +0200

    Refaktorering av token provider
    Fjernet service to service token provider, bruker heller scoped token og sett scope i environment variabler

--- tools/token_provider/src/main/kotlin/no/nav/amt/tiltak/tools/token_provider/ServiceToServiceTokenProvider.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.amt.tiltak.tools.token_provider
-
-/**
- * Provides tokens for service to service authentication where each services requires a specific token
- */
-interface ServiceToServiceTokenProvider {
-	fun getServiceToken(serviceName: String, namespace: String, cluster: String): String
-}
