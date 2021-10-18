d55ce06d067680913d0899b51dd7cfe0ce1f0ac4
Einar Bjerve <einar.bjerve@nav.no>
2021-10-18 15:27:52 +0200

    Bytta ut token-support for system til system-token.
    Co-authored-by: Izis Frøyhov <izis.froyhov@nav.no>

--- application/src/main/kotlin/no/nav/amt/tiltak/application/ApplicationConfig.kt
+++ application/src/main/kotlin/no/nav/amt/tiltak/application/ApplicationConfig.kt
@@ -1,15 +1,18 @@
 package no.nav.amt.tiltak.application
 
+import no.nav.amt.tiltak.connectors.nom.client.NomConfig
 import no.nav.amt.tiltak.tools.token_provider.ScopedTokenProvider
 import no.nav.amt.tiltak.tools.token_provider.azure_ad.AzureAdScopedTokenProviderBuilder
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
 
 @Profile("!local")
 @EnableJwtTokenValidation
 @Configuration
+@Import(NomConfig::class)
 open class ApplicationConfig {
 
 	@Bean
--- connectors/nom/src/main/kotlin/no/nav/amt/tiltak/connectors/nom/client/NomConfig.kt
+++ connectors/nom/src/main/kotlin/no/nav/amt/tiltak/connectors/nom/client/NomConfig.kt
@@ -1,28 +1,22 @@
 package no.nav.amt.tiltak.connectors.nom.client
 
 import no.nav.amt.tiltak.core.port.NomConnector
-import no.nav.security.token.support.client.core.ClientProperties
-import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
-import no.nav.security.token.support.client.spring.ClientConfigurationProperties
-import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import no.nav.amt.tiltak.tools.token_provider.ScopedTokenProvider
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.function.Supplier
 
-@EnableOAuth2Client(cacheEnabled=true)
 @Configuration
 open class NomConfig {
 
 	@Bean("nomApiTokenSupplier")
 	open fun tokenSupplier (
-		clientConfigProps: ClientConfigurationProperties,
-		accessTokenService: OAuth2AccessTokenService ) : Supplier<String> {
-		val clientProperties: ClientProperties = clientConfigProps.registration["nom"]
-			?: throw RuntimeException("could not find oauth2 client config for nom")
-
-		return Supplier { accessTokenService.getAccessToken(clientProperties).accessToken }
+		tokenProvider: ScopedTokenProvider,
+		@Value("nom.scope") scope: String
+	) : Supplier<String> {
+		return Supplier { tokenProvider.getToken(scope) }
 	}
 
 	@Bean
