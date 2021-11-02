9db36495b47a08f9e9d63ecf46ecb59254ced6c5
Magnus Evensberget <magnus.evensberget@nav.no>
2021-11-02 21:15:09 +0100

    QA: Ferdig Tiltaksleverandor

--- tiltaksleverandor/src/main/kotlin/no/nav/amt/tiltak/tiltaksleverandor/repositories/TiltaksleverandorRepository.kt
+++ tiltaksleverandor/src/main/kotlin/no/nav/amt/tiltak/tiltaksleverandor/repositories/TiltaksleverandorRepository.kt
@@ -1,16 +1,30 @@
 package no.nav.amt.tiltak.tiltaksleverandor.repositories
 
 import no.nav.amt.tiltak.tiltaksleverandor.dbo.TiltaksleverandorDbo
-import no.nav.amt.tiltak.tiltaksleverandor.repositories.statements.insert.TiltaksleverandorInsertStatement
-import no.nav.amt.tiltak.tiltaksleverandor.repositories.statements.parts.TiltaksleverandorVirksomhetsnummerEqualsQueryPart
-import no.nav.amt.tiltak.tiltaksleverandor.repositories.statements.query.GetTiltaksleverandorQueryStatement
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 open class TiltaksleverandorRepository(
 	private val template: NamedParameterJdbcTemplate,
 ) {
+
+	private val rowMapper = RowMapper { rs, _ ->
+		TiltaksleverandorDbo(
+			internalId = rs.getInt("id"),
+			externalId = UUID.fromString(rs.getString("external_id")),
+			organisasjonsnummer = rs.getString("organisasjonsnummer"),
+			organisasjonsnavn = rs.getString("organisasjonsnavn"),
+			virksomhetsnummer = rs.getString("virksomhetsnummer"),
+			virksomhetsnavn = rs.getString("virksomhetsnavn"),
+			createdAt = rs.getTimestamp("created_at").toLocalDateTime(),
+			modifiedAt = rs.getTimestamp("modified_at").toLocalDateTime()
+		)
+	}
+
 
 	fun insert(
 		organisasjonsnavn: String,
@@ -24,22 +38,54 @@ open class TiltaksleverandorRepository(
 			return savedTiltaksleverandor
 		}
 
-		TiltaksleverandorInsertStatement(
-			template = template,
-			organisasjonsnummer = organisasjonsnummer,
-			organisasjonsnavn = organisasjonsnavn,
-			virksomhetsnummer = virksomhetsnummer,
-			virksomhetsnavn = virksomhetsnavn
-		).execute()
+		val sql = """
+			INSERT INTO tiltaksleverandor(external_id, organisasjonsnummer, organisasjonsnavn, virksomhetsnummer, virksomhetsnavn)
+			VALUES (:externalId,
+					:organisasjonsnummer,
+					:organisasjonsnavn,
+					:virksomhetsnummer,
+					:virksomhetsnavn)
+		""".trimIndent()
+
+		val externalId = UUID.randomUUID()
+
+		val parameters = MapSqlParameterSource().addValues(
+			mapOf(
+				"externalId" to externalId,
+				"organisasjonsnummer" to organisasjonsnummer,
+				"organisasjonsnavn" to organisasjonsnavn,
+				"virksomhetsnummer" to virksomhetsnummer,
+				"virksomhetsnavn" to virksomhetsnavn
+			)
+		)
+
+		template.update(sql, parameters)
 
 		return getByVirksomhetsnummer(virksomhetsnummer)
 			?: throw NoSuchElementException("Virksomhet med virksomhetsnummer $virksomhetsnummer finnes ikke")
 	}
 
 	fun getByVirksomhetsnummer(virksomhetsnummer: String): TiltaksleverandorDbo? {
-		return GetTiltaksleverandorQueryStatement(template)
-			.addPart(TiltaksleverandorVirksomhetsnummerEqualsQueryPart(virksomhetsnummer))
-			.execute()
+		val sql = """
+			SELECT id,
+				   external_id,
+				   organisasjonsnummer,
+				   organisasjonsnavn,
+				   virksomhetsnummer,
+				   virksomhetsnavn,
+				   created_at,
+				   modified_at
+			FROM tiltaksleverandor
+			WHERE virksomhetsnummer = :virksomhetsnummer
+		""".trimIndent()
+
+		val parameters = MapSqlParameterSource().addValues(
+			mapOf(
+				"virksomhetsnummer" to virksomhetsnummer
+			)
+		)
+
+		return template.query(sql, parameters, rowMapper)
 			.firstOrNull()
 	}
 
