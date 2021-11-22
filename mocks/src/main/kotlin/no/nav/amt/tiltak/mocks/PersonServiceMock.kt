4e15fa31865a9b894fc4a6a45afa6f56cdaa7914
Magnus Evensberget <magnus.evensberget@nav.no>
2021-11-22 20:06:38 +0100

    86: Arenaingest: Koble connector for hentVeileder i DeltagerService
    Fikset byggfeil grunnet feilende test, oppdatert mock

--- mocks/src/main/kotlin/no/nav/amt/tiltak/mocks/PersonServiceMock.kt
+++ mocks/src/main/kotlin/no/nav/amt/tiltak/mocks/PersonServiceMock.kt
@@ -1,5 +1,6 @@
 package no.nav.amt.tiltak.mocks
 
+import no.nav.amt.tiltak.core.domain.veileder.Veileder
 import no.nav.amt.tiltak.core.port.Kontaktinformasjon
 import no.nav.amt.tiltak.core.port.Person
 import no.nav.amt.tiltak.core.port.PersonService
@@ -19,6 +20,10 @@ class PersonServiceMock : PersonService {
 	override fun hentPerson(fnr: String): Person {
 		return PersonMockDataProvider.getPersonByFodselsnummer(fnr)?.toPdlBruker()
 			?: throw RuntimeException("Mock inneholder ikke informasjon om bruker med fødselsnummer $fnr")
+	}
+
+	override fun hentVeileder(fnr: String): Veileder? {
+		throw NotImplementedError("Mock does not support getting Veileder")
 	}
 
 	private fun PersonMockData.toPdlBruker(): Person {
