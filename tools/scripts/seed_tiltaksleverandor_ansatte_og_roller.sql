a581e0670ea914d7a81ac8dc3373885999d3711a
Alexander Gård <alex.gaard@hotmail.com>
2021-11-26 14:33:11 +0100

    Startet på seed script for tiltaksleverandor ansatt og rolle

---
+++ tools/scripts/seed_tiltaksleverandor_ansatte_og_roller.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO tiltaksleverandor_ansatt(id, personlig_ident, fornavn, etternavn, telefonnummer, epost)
+    VALUES ('8cfdb592-dc54-4339-9209-3e7400024c14', '07055400886', 'Gina', 'Rodger', null, null);
+
+INSERT INTO tiltaksleverandor_ansatt_rolle(id, ansatt_id, tiltaksleverandor_id, rolle)
+    VALUES ('e721d79d-8034-4d26-980e-d883ac7a0e36', '8cfdb592-dc54-4339-9209-3e7400024c14', 'bd92c307-523f-41d3-b3e0-4449d5139753', 'KOORDINATOR');
+
