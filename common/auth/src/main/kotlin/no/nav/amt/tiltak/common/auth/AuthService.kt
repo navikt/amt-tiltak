81a2dc9e333ead73c47c6d5186214a2a8f42ba7a
Alexander Gård <alex.gaard@hotmail.com>
2021-11-29 11:37:53 +0100

    Flyttet autentiseringslogikk ut i egen service
    Co-authored-by: Magnus Evensberget <magnus.evensberget@nav.no>

---
+++ common/auth/src/main/kotlin/no/nav/amt/tiltak/common/auth/AuthService.kt
@@ -0,0 +1,26 @@
+package no.nav.amt.tiltak.common.auth
+
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+open class AuthService(
+	private val tokenValidationContextHolder: TokenValidationContextHolder
+) {
+
+	open fun hentPersonligIdentTilInnloggetBruker(): String {
+		val context = tokenValidationContextHolder.tokenValidationContext
+
+		val token = context.firstValidToken.orElseThrow {
+			throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "User is not authorized, valid token is missing")
+		}
+
+		return token.jwtTokenClaims["pid"]?.toString() ?: throw ResponseStatusException(
+			HttpStatus.UNAUTHORIZED,
+			"PID is missing or is not a string"
+		)
+	}
+
+}
