bf665d2ecd1c0cd4a04f5edebdfafc29311d39d9
Alexander Gård <alex.gaard@hotmail.com>
2021-12-01 12:00:40 +0100

    Rename NomConnector -> NomClient
    Byttet også bruk av Veileder til NomVeileder

--- connectors/nom/src/main/kotlin/no/nav/amt/tiltak/connectors/nom/client/NomClientMock.kt
+++ connectors/nom/src/main/kotlin/no/nav/amt/tiltak/connectors/nom/client/NomClientMock.kt
@@ -1,16 +1,13 @@
 package no.nav.amt.tiltak.connectors.nom.client
 
-import no.nav.amt.tiltak.core.domain.veileder.Veileder
-import no.nav.amt.tiltak.core.port.NomConnector
+class NomClientMock : NomClient {
 
-class NomClientMock : NomConnector {
-
-	override fun hentVeileder(navIdent: String): Veileder? {
-		return Veileder(
+	override fun hentVeileder(navIdent: String): NomVeileder {
+		return NomVeileder(
 			navIdent = navIdent,
 			fornavn = "F_$navIdent",
 			etternavn = "E_$navIdent",
-			visningsNavn = "F_$navIdent E_$navIdent",
+			visningNavn = "F_$navIdent E_$navIdent",
 			epost = "F_$navIdent.E_$navIdent@trygdeetaten.no",
 		)
 	}
