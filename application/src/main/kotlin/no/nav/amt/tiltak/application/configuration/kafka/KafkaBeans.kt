381ba35fafa9c7b87c77391da83541d6781106ee
izisfro <izis.froyhov@nav.no>
2021-12-15 10:12:15 +0100

    Merger script og resetter kafka offset for å fikse deploy

--- application/src/main/kotlin/no/nav/amt/tiltak/application/configuration/kafka/KafkaBeans.kt
+++ application/src/main/kotlin/no/nav/amt/tiltak/application/configuration/kafka/KafkaBeans.kt
@@ -16,7 +16,7 @@ open class KafkaBeans {
     open fun kafkaConsumerProperties(): KafkaProperties {
         return object : KafkaProperties {
             override fun consumer(): Properties {
-                return KafkaPropertiesPreset.aivenDefaultConsumerProperties("amt-tiltak-consumer.v2")
+                return KafkaPropertiesPreset.aivenDefaultConsumerProperties("amt-tiltak-consumer.v3")
             }
 
             override fun producer(): Properties {
