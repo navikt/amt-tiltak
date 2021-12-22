4df48e6547c77e343c4cdc7baf539509b646a214
Einar Bjerve <einar.bjerve@nav.no>
2021-12-22 08:41:13 +0100

    Oppdatert konvertering av arena-status (#71)
    * Oppdatert konvertering av arena-status

--- ingestors/arena-ingestor/src/test/kotlin/no/nav/amt/tiltak/ingestors/arena/processors/DeltakerStatusConverterTest.kt
+++ ingestors/arena-ingestor/src/test/kotlin/no/nav/amt/tiltak/ingestors/arena/processors/DeltakerStatusConverterTest.kt
@@ -31,109 +31,185 @@ class DeltakerStatusConverterTest : StringSpec({
 
 	val converter = DeltakerStatusConverter(SimpleMeterRegistry())
 
-	"status - AKTUELL og mangler startdato - returnerer IKKE_AKTUELL" {
-		converter.convert("AKTUELL", null, null) shouldBe IKKE_AKTUELL
+	"status - AKTUELL - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("AKTUELL", null, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - AKTUELL og har startdato i fortid - returnerer GJENNOMFORES" {
+		converter.convert("AKTUELL", yesterday, null, null) shouldBe GJENNOMFORES
+	}
+	"status - AKTUELL og har startdato i fremtid - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("AKTUELL", tomorrow, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - AKTUELL og har sluttdato i fortid - returnerer HAR_SLUTTET" {
+		converter.convert("AKTUELL", yesterday.minusDays(1), yesterday, null) shouldBe HAR_SLUTTET
+	}
+	"status - AKTUELL og har sluttdato i fremtid - returnerer GJENNOMFORES" {
+		converter.convert("AKTUELL", yesterday, tomorrow, null) shouldBe GJENNOMFORES
 	}
 
-
 	"status - AVSLAG og mangler startdato - returnerer IKKE_AKTUELL" {
-		converter.convert("AVSLAG", null, null) shouldBe IKKE_AKTUELL
+		converter.convert("AVSLAG", null, null, null) shouldBe IKKE_AKTUELL
 	}
 
 
 	"status - DELAVB og mangler startdato - returnerer IKKE_AKTUELL" {
-		converter.convert("DELAVB", null, null) shouldBe IKKE_AKTUELL
+		converter.convert("DELAVB", null, null, null) shouldBe IKKE_AKTUELL
 	}
-	"status - DELAVB og har startdato i fortid - returnerer HAR_SLUTTET" {
-		converter.convert("DELAVB", yesterday, null) shouldBe HAR_SLUTTET
+	"status - DELAVB og har startdato før endretdato - returnerer HAR_SLUTTET" {
+		converter.convert("DELAVB", yesterday.minusDays(1), null, yesterday) shouldBe HAR_SLUTTET
+	}
+	"status - DELAVB og har startdato etter endretdato - returnerer IKKE_AKTUELL" {
+		converter.convert("DELAVB", yesterday, null, yesterday.minusDays(1)) shouldBe IKKE_AKTUELL
 	}
 	"status - DELAVB og har startdato i fremtid - returnerer IKKE_AKTUELL" {
-		converter.convert("DELAVB", tomorrow, null) shouldBe IKKE_AKTUELL
+		converter.convert("DELAVB", tomorrow, null, null) shouldBe IKKE_AKTUELL
 	}
 
-
-	"status - FULLF - returnerer HAR_SLUTTET" {
-		converter.convert("FULLF", null, null) shouldBe HAR_SLUTTET
+	"status - FULLF og mangler startdato - returnerer IKKE_AKTUELL" {
+		converter.convert("FULLF", null, null, null) shouldBe IKKE_AKTUELL
 	}
-
+	"status - FULLF og har startdato før endretdato - returnerer HAR_SLUTTET" {
+		converter.convert("FULLF", yesterday.minusDays(1), null, yesterday) shouldBe HAR_SLUTTET
+	}
+	"status - FULLF og har startdato etter endretdato - returnerer IKKE_AKTUELL" {
+		converter.convert("FULLF", yesterday, null, yesterday.minusDays(1)) shouldBe IKKE_AKTUELL
+	}
+	"status - FULLF og har startdato i fremtid - returnerer IKKE_AKTUELL" {
+		converter.convert("FULLF", tomorrow, null, null) shouldBe IKKE_AKTUELL
+	}
 
 	"status - GJENN - returnerer VENTER_PÅ_OPPSTART" {
-		converter.convert("GJENN", null, null) shouldBe VENTER_PA_OPPSTART
+		converter.convert("GJENN", null, null, null) shouldBe VENTER_PA_OPPSTART
 	}
 	"status - GJENN og har startdato i fortid - returnerer GJENNOMFORES" {
-		converter.convert("GJENN", yesterday, null) shouldBe GJENNOMFORES
+		converter.convert("GJENN", yesterday, null, null) shouldBe GJENNOMFORES
 	}
 	"status - GJENN og har startdato i fremtid - returnerer VENTER_PÅ_OPPSTART" {
-		converter.convert("GJENN", tomorrow, null) shouldBe VENTER_PA_OPPSTART
+		converter.convert("GJENN", tomorrow, null, null) shouldBe VENTER_PA_OPPSTART
 	}
 	"status - GJENN og har sluttdato i fortid - returnerer HAR_SLUTTET" {
-		converter.convert("GJENN", yesterday.minusDays(1), yesterday) shouldBe HAR_SLUTTET
+		converter.convert("GJENN", yesterday.minusDays(1), yesterday, null) shouldBe HAR_SLUTTET
 	}
 	"status - GJENN og har sluttdato i fremtid - returnerer GJENNOMFORES" {
-		converter.convert("GJENN", yesterday, tomorrow) shouldBe GJENNOMFORES
+		converter.convert("GJENN", yesterday, tomorrow, null) shouldBe GJENNOMFORES
 	}
 
 
 	"status - GJENN_AVB og mangler startdato - returnerer IKKE_AKTUELL" {
-		converter.convert("GJENN_AVB", null, null) shouldBe IKKE_AKTUELL
+		converter.convert("GJENN_AVB", null, null, null) shouldBe IKKE_AKTUELL
 	}
-	"status - GJENN_AVB og har startdato i fortid - returnerer HAR_SLUTTET" {
-		converter.convert("GJENN_AVB", yesterday, null) shouldBe HAR_SLUTTET
+	"status - GJENN_AVB og har startdato før endretdato - returnerer HAR_SLUTTET" {
+		converter.convert("GJENN_AVB", yesterday.minusDays(1), null, yesterday) shouldBe HAR_SLUTTET
+	}
+	"status - GJENN_AVB og har startdato etter endretdato - returnerer IKKE_AKTUELL" {
+		converter.convert("GJENN_AVB", yesterday, null, yesterday.minusDays(1)) shouldBe IKKE_AKTUELL
 	}
 	"status - GJENN_AVB og har startdato i fremtid - returnerer IKKE_AKTUELL" {
-		converter.convert("GJENN_AVB", tomorrow, null) shouldBe IKKE_AKTUELL
+		converter.convert("GJENN_AVB", tomorrow, null, null) shouldBe IKKE_AKTUELL
 	}
 
 
 	"status - GJENN_AVL og mangler startdato - returnerer IKKE_AKTUELL" {
-		converter.convert("GJENN_AVL", null, null) shouldBe IKKE_AKTUELL
+		converter.convert("GJENN_AVL", null, null, null) shouldBe IKKE_AKTUELL
 	}
-	"status - GJENN_AVL og har startdato i fortid - returnerer HAR_SLUTTET" {
-		converter.convert("GJENN_AVL", yesterday, null) shouldBe HAR_SLUTTET
+	"status - GJENN_AVL og har startdato før endretdato - returnerer HAR_SLUTTET" {
+		converter.convert("GJENN_AVL", yesterday.minusDays(1), null, yesterday) shouldBe HAR_SLUTTET
+	}
+	"status - GJENN_AVL og har startdato etter endretdato - returnerer IKKE_AKTUELL" {
+		converter.convert("GJENN_AVL", yesterday, null, yesterday.minusDays(1)) shouldBe IKKE_AKTUELL
 	}
 	"status - GJENN_AVL og har startdato i fremtid - returnerer IKKE_AKTUELL" {
-		converter.convert("GJENN_AVL", tomorrow, null) shouldBe IKKE_AKTUELL
+		converter.convert("GJENN_AVL", tomorrow, null, null) shouldBe IKKE_AKTUELL
 	}
 
 
 	"status - IKKAKTUELL - returnerer IKKE_AKTUELL" {
-		converter.convert("IKKAKTUELL", null, null) shouldBe IKKE_AKTUELL
+		converter.convert("IKKAKTUELL", null, null, null) shouldBe IKKE_AKTUELL
 	}
 
 
 	"status - IKKEM og mangler startdato - returnerer IKKE_AKTUELL" {
-		converter.convert("IKKEM", null, null) shouldBe IKKE_AKTUELL
+		converter.convert("IKKEM", null, null, null) shouldBe IKKE_AKTUELL
 	}
-	"status - IKKEM og har startdato i fortid - returnerer HAR_SLUTTET" {
-		converter.convert("IKKEM", yesterday, null) shouldBe HAR_SLUTTET
+	"status - IKKEM og har startdato før endretdato - returnerer HAR_SLUTTET" {
+		converter.convert("IKKEM", yesterday.minusDays(1), null, yesterday) shouldBe HAR_SLUTTET
+	}
+	"status - IKKEM og har startdato etter endretdato - returnerer IKKE_AKTUELL" {
+		converter.convert("IKKEM", yesterday, null, yesterday.minusDays(1)) shouldBe IKKE_AKTUELL
 	}
 	"status - IKKEM og har startdato i fremtid - returnerer IKKE_AKTUELL" {
-		converter.convert("IKKEM", tomorrow, null) shouldBe IKKE_AKTUELL
+		converter.convert("IKKEM", tomorrow, null, null) shouldBe IKKE_AKTUELL
 	}
 
 
-	"status - INFOMOETE - returnerer IKKE_AKTUELL" {
-		converter.convert("INFOMOETE", null, null) shouldBe IKKE_AKTUELL
+	"status - INFOMOETE - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("INFOMOETE", null, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - INFOMOETE og har startdato i fortid - returnerer GJENNOMFORES" {
+		converter.convert("INFOMOETE", yesterday, null, null) shouldBe GJENNOMFORES
+	}
+	"status - INFOMOETE og har startdato i fremtid - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("INFOMOETE", tomorrow, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - INFOMOETE og har sluttdato i fortid - returnerer HAR_SLUTTET" {
+		converter.convert("INFOMOETE", yesterday.minusDays(1), yesterday, null) shouldBe HAR_SLUTTET
+	}
+	"status - INFOMOETE og har sluttdato i fremtid - returnerer GJENNOMFORES" {
+		converter.convert("INFOMOETE", yesterday, tomorrow, null) shouldBe GJENNOMFORES
 	}
 
-
-	"status - JATAKK - returnerer IKKE_AKTUELL" {
-		converter.convert("JATAKK", null, null) shouldBe IKKE_AKTUELL
+	"status - JATAKK - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("JATAKK", null, null, null) shouldBe VENTER_PA_OPPSTART
 	}
-
+	"status - JATAKK og har startdato i fortid - returnerer GJENNOMFORES" {
+		converter.convert("JATAKK", yesterday, null, null) shouldBe GJENNOMFORES
+	}
+	"status - JATAKK og har startdato i fremtid - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("JATAKK", tomorrow, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - JATAKK og har sluttdato i fortid - returnerer HAR_SLUTTET" {
+		converter.convert("JATAKK", yesterday.minusDays(1), yesterday, null) shouldBe HAR_SLUTTET
+	}
+	"status - JATAKK og har sluttdato i fremtid - returnerer GJENNOMFORES" {
+		converter.convert("JATAKK", yesterday, tomorrow, null) shouldBe GJENNOMFORES
+	}
 
 	"status - NEITAKK - returnerer IKKE_AKTUELL" {
-		converter.convert("NEITAKK", null, null) shouldBe IKKE_AKTUELL
+		converter.convert("NEITAKK", null, null, null) shouldBe IKKE_AKTUELL
 	}
 
 
 	"status - TILBUD - returnerer VENTER_PÅ_OPPSTART" {
-		converter.convert("TILBUD", null, null) shouldBe VENTER_PA_OPPSTART
+		converter.convert("TILBUD", null, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - TILBUD og har startdato i fortid - returnerer GJENNOMFORES" {
+		converter.convert("TILBUD", yesterday, null, null) shouldBe GJENNOMFORES
+	}
+	"status - TILBUD og har startdato i fremtid - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("TILBUD", tomorrow, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - TILBUD og har sluttdato i fortid - returnerer HAR_SLUTTET" {
+		converter.convert("TILBUD", yesterday.minusDays(1), yesterday, null) shouldBe HAR_SLUTTET
+	}
+	"status - TILBUD og har sluttdato i fremtid - returnerer GJENNOMFORES" {
+		converter.convert("TILBUD", yesterday, tomorrow, null) shouldBe GJENNOMFORES
 	}
 
 
-	"status - VENTELISTE - returnerer IKKE_AKTUELL" {
-		converter.convert("VENTELISTE", null, null) shouldBe IKKE_AKTUELL
+	"status - VENTELISTE - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("VENTELISTE", null, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - VENTELISTE og har startdato i fortid - returnerer GJENNOMFORES" {
+		converter.convert("VENTELISTE", yesterday, null, null) shouldBe GJENNOMFORES
+	}
+	"status - VENTELISTE og har startdato i fremtid - returnerer VENTER_PÅ_OPPSTART" {
+		converter.convert("VENTELISTE", tomorrow, null, null) shouldBe VENTER_PA_OPPSTART
+	}
+	"status - VENTELISTE og har sluttdato i fortid - returnerer HAR_SLUTTET" {
+		converter.convert("VENTELISTE", yesterday.minusDays(1), yesterday, null) shouldBe HAR_SLUTTET
+	}
+	"status - VENTELISTE og har sluttdato i fremtid - returnerer GJENNOMFORES" {
+		converter.convert("VENTELISTE", yesterday, tomorrow, null) shouldBe GJENNOMFORES
 	}
 
 })
