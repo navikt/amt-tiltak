24363cd72ce715ab60f556ba51df73afb1f11774
Alexander Gård <alex.gaard@hotmail.com>
2022-02-04 14:54:50 +0100

    Reset kafka consumer offset

--- application/src/main/kotlin/no/nav/amt/tiltak/application/configuration/kafka/KafkaBeans.kt
+++ application/src/main/kotlin/no/nav/amt/tiltak/application/configuration/kafka/KafkaBeans.kt
@@ -16,7 +16,7 @@ open class KafkaBeans {
     open fun kafkaConsumerProperties(): KafkaProperties {
         return object : KafkaProperties {
             override fun consumer(): Properties {
-                return KafkaPropertiesPreset.aivenDefaultConsumerProperties("amt-tiltak-consumer.v7")
+                return KafkaPropertiesPreset.aivenDefaultConsumerProperties("amt-tiltak-consumer.v8")
             }
 
             override fun producer(): Properties {
