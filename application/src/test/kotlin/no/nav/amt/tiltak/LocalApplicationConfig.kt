0dc420ea40a0f0fc40136156d59cbb271124c317
Alexander Gård <AlexGaard@users.noreply.github.com>
2022-02-16 12:00:08 +0100

    Gjør klar for å koble til med lokal frontend (#132)
    * La til åpen CORS
    * Seed database under oppstart
    * Tweaket på token claims og expiry
    * Ikke clean flyway_schema_history med DatabaseTestUtils

--- application/src/test/kotlin/no/nav/amt/tiltak/LocalApplicationConfig.kt
+++ application/src/test/kotlin/no/nav/amt/tiltak/LocalApplicationConfig.kt
@@ -4,6 +4,8 @@ import no.nav.amt.tiltak.tools.token_provider.ScopedTokenProvider
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @EnableJwtTokenValidation
 @Configuration
@@ -14,6 +16,19 @@ open class LocalApplicationConfig {
 		return object : ScopedTokenProvider {
 			override fun getToken(scope: String): String {
 				return "MOCK_TOKEN"
+			}
+		}
+	}
+
+	@Bean
+	open fun corsConfigurer(): WebMvcConfigurer {
+		return object : WebMvcConfigurer {
+			override fun addCorsMappings(registry: CorsRegistry) {
+				registry
+					.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedHeaders("*")
+					.allowedMethods("*")
 			}
 		}
 	}
