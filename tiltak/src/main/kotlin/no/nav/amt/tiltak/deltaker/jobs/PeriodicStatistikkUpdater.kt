4f2e5fa5997ecd4eb73668a32ff581c397b3ea68
Einar Bjerve <einar.bjerve@nav.no>
2022-02-16 12:58:28 +0100

    La til manglende open

--- tiltak/src/main/kotlin/no/nav/amt/tiltak/deltaker/jobs/PeriodicStatistikkUpdater.kt
+++ tiltak/src/main/kotlin/no/nav/amt/tiltak/deltaker/jobs/PeriodicStatistikkUpdater.kt
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class PeriodicStatistikkUpdater(
+open class PeriodicStatistikkUpdater(
 	private val deltakerStatistikkUpdater: DeltakerStatistikkUpdater
 ) {
 
