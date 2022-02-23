a75e98cde6b84e3b56fe89d8fda22cd0ee5c36e2
Alexander Gård <alex.gaard@hotmail.com>
2022-02-23 09:19:11 +0100

    Log hvilken norg enhet som feiler

--- clients/norg/src/main/kotlin/no/nav/amt/tiltak/clients/norg/NorgClientImpl.kt
+++ clients/norg/src/main/kotlin/no/nav/amt/tiltak/clients/norg/NorgClientImpl.kt
@@ -21,7 +21,7 @@ class NorgClientImpl(
 
 		httpClient.newCall(request).execute().use { response ->
 			if (!response.isSuccessful) {
-				throw RuntimeException("Klarte ikke å hente enhet fra norg. Status: ${response.code}")
+				throw RuntimeException("Klarte ikke å hente enhet enhetId=$enhetId fra norg status=${response.code}")
 			}
 
 			val body = response.body?.string() ?: throw RuntimeException("Body is missing")
