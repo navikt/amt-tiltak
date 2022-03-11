16d78592d36940e53ea4e5b6bccaa1c6f53f9856
izisfro <izis.froyhov@nav.no>
2022-03-11 11:25:47 +0100

    Test

--- test/database/src/main/kotlin/no/nav/amt/tiltak/test/database/data/commands/InsertTiltakCommand.kt
+++ test/database/src/main/kotlin/no/nav/amt/tiltak/test/database/data/commands/InsertTiltakCommand.kt
@@ -1,9 +1,19 @@
 package no.nav.amt.tiltak.test.database.data.commands
 
+import no.nav.amt.tiltak.core.domain.tiltak.Tiltak
 import java.util.*
 
 data class InsertTiltakCommand(
 	val id: UUID,
 	val navn: String,
 	val type: String
-)
+) {
+	fun toTiltak(): Tiltak {
+		return Tiltak(
+			id = this.id,
+			kode = this.type,
+			navn = this.navn
+		)
+	}
+}
+
