b4153a550b631be579214b13bd8439ae09dbdbfe
Alexander Gård <alex.gaard@hotmail.com>
2022-05-05 13:16:05 +0200

    Endepunkt for å markere endringsmelding som ferdig

--- tiltak/src/main/kotlin/no/nav/amt/tiltak/endringsmelding/EndringsmeldingService.kt
+++ tiltak/src/main/kotlin/no/nav/amt/tiltak/endringsmelding/EndringsmeldingService.kt
@@ -1,7 +1,9 @@
 package no.nav.amt.tiltak.endringsmelding
 
 import no.nav.amt.tiltak.core.domain.tiltak.Endringsmelding
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDate
 import java.util.*
 
@@ -11,8 +13,23 @@ open class EndringsmeldingService(
 	private val endringsmeldingQuery: EndringsmeldingForGjennomforingQuery,
 ) {
 
+	open fun hentEndringsmelding(id: UUID): EndringsmeldingDbo {
+		return endringsmeldingRepository.get(id)
+	}
+
 	open fun opprettMedStartDato(deltakerId: UUID, startDato: LocalDate, ansattId: UUID): EndringsmeldingDbo {
 		return endringsmeldingRepository.insertOgInaktiverStartDato(startDato, deltakerId, ansattId)
+	}
+
+	open fun markerSomFerdig(endringsmeldingId: UUID, navAnsattId: UUID) {
+		val endringsmelding = endringsmeldingRepository.get(endringsmeldingId)
+
+		if (!endringsmelding.aktiv) {
+			// Kast custom exception og map til 400
+			throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Endringsmelding er ikke aktiv")
+		}
+
+		endringsmeldingRepository.markerSomFerdig(endringsmeldingId, navAnsattId)
 	}
 
 	open fun hentEndringsmeldinger(gjennomforingId: UUID) : List<Endringsmelding> {
--- tiltak/src/test/kotlin/no/nav/amt/tiltak/endringsmelding/EndringsmeldingNavControllerTest.kt
+++ tiltak/src/test/kotlin/no/nav/amt/tiltak/endringsmelding/EndringsmeldingNavControllerTest.kt
@@ -2,14 +2,18 @@ package no.nav.amt.tiltak.endringsmelding
 
 import no.nav.amt.tiltak.common.auth.AuthService
 import no.nav.amt.tiltak.core.domain.arrangor.Ansatt
-import no.nav.amt.tiltak.core.domain.tiltak.Bruker
-import no.nav.amt.tiltak.core.domain.tiltak.Endringsmelding
+import no.nav.amt.tiltak.core.domain.nav_ansatt.NavAnsatt
+import no.nav.amt.tiltak.core.domain.tiltak.*
+import no.nav.amt.tiltak.core.port.DeltakerService
+import no.nav.amt.tiltak.core.port.NavAnsattService
 import no.nav.amt.tiltak.core.port.TiltaksansvarligTilgangService
 import no.nav.amt.tiltak.test.mock_oauth_server.MockOAuthServer
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -35,6 +39,12 @@ class EndringsmeldingNavControllerTest {
 
 	@MockBean
 	private lateinit var endringsmeldingService: EndringsmeldingService
+
+	@MockBean
+	private lateinit var navAnsattService: NavAnsattService
+
+	@MockBean
+	private lateinit var deltakerService: DeltakerService
 
 	companion object : MockOAuthServer() {
 		@AfterAll
@@ -121,6 +131,73 @@ class EndringsmeldingNavControllerTest {
 
 		assertEquals(expectedJson, response.contentAsString)
 		assertEquals(200, response.status)
+	}
+
+	@Test
+	fun `markerFerdig() - skal returnere 401 hvis token mangler`() {
+		val response = mockMvc.perform(
+			MockMvcRequestBuilders.patch("/api/nav-ansatt/endringsmelding/${UUID.randomUUID()}/ferdig")
+		).andReturn().response
+
+		assertEquals(401, response.status)
+	}
+
+	@Test
+	fun `markerFerdig() - skal returnere 200 og markere som ferdig`() {
+		val endringsmeldingId = UUID.randomUUID()
+		val gjennomforingId = UUID.randomUUID()
+		val deltakerId = UUID.randomUUID()
+		val navAnsattId = UUID.randomUUID()
+		val navIdent = "a12345"
+
+		Mockito.`when`(authService.hentNavIdentTilInnloggetBruker())
+			.thenReturn(navIdent)
+
+		Mockito.`when`(endringsmeldingService.hentEndringsmelding(endringsmeldingId))
+			.thenReturn(EndringsmeldingDbo(
+				id = endringsmeldingId,
+				deltakerId = deltakerId,
+				startDato = null,
+				godkjentAvNavAnsatt = null,
+				aktiv = true,
+				opprettetAvId = UUID.randomUUID(),
+				createdAt = LocalDateTime.now(),
+				modifiedAt = LocalDateTime.now()
+			))
+
+		Mockito.`when`(deltakerService.hentDeltaker(deltakerId))
+			.thenReturn(Deltaker(
+				id = UUID.randomUUID(),
+				gjennomforingId = gjennomforingId,
+				bruker = null,
+				startDato = null,
+				sluttDato = null,
+				statuser = DeltakerStatuser(listOf(DeltakerStatus.nyAktiv(Deltaker.Status.DELTAR))),
+				registrertDato = LocalDateTime.now(),
+				dagerPerUke = null,
+				prosentStilling = null,
+			))
+
+		Mockito.`when`(tiltaksansvarligTilgangService.harTilgangTilGjennomforing(navIdent, gjennomforingId))
+			.thenReturn(true)
+
+		Mockito.`when`(navAnsattService.getNavAnsatt(navIdent))
+			.thenReturn(NavAnsatt(
+				id = navAnsattId,
+				navIdent = navIdent,
+				navn = "",
+				epost = null,
+				telefonnummer = null,
+			))
+
+		val response = mockMvc.perform(
+			MockMvcRequestBuilders.patch("/api/nav-ansatt/endringsmelding/$endringsmeldingId/ferdig")
+				.header("Authorization", "Bearer ${azureAdToken()}")
+		).andReturn().response
+
+		assertEquals(200, response.status)
+
+		verify(endringsmeldingService, times(1)).markerSomFerdig(endringsmeldingId, navAnsattId)
 	}
 
 }
