66f561ead1b6821a3d2a7d495cfbeba05fcc385b
Alexander Gård <alex.gaard@hotmail.com>
2022-05-25 12:43:25 +0200

    La til default true for skjermet person og felles secure log

--- tilgangskontroll/src/main/kotlin/no/nav/amt/tiltak/tilgangskontroll/skjermet_person/SkjermetPersonService.kt
+++ tilgangskontroll/src/main/kotlin/no/nav/amt/tiltak/tilgangskontroll/skjermet_person/SkjermetPersonService.kt
@@ -2,6 +2,7 @@ package no.nav.amt.tiltak.tilgangskontroll.skjermet_person
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.amt.tiltak.clients.poao_tilgang.PoaoTilgangClient
+import no.nav.amt.tiltak.log.SecureLog.secureLog
 import no.nav.amt.tiltak.tilgangskontroll.utils.CacheUtils.tryCacheFirstNotNull
 import org.springframework.stereotype.Service
 import java.time.Duration
@@ -18,7 +19,10 @@ class SkjermetPersonService(
 
 	fun erSkjermet(norskIdent: String): Boolean {
 		return tryCacheFirstNotNull(norskIdentToErSkjermetCache, norskIdent) {
-			poaoTilgangClient.erSkjermet(listOf(norskIdent)).getOrDefault(norskIdent, false)
+			poaoTilgangClient.erSkjermet(listOf(norskIdent)).getOrElse(norskIdent) {
+				secureLog.warn("Mangler data for skjermet person med fnr=$norskIdent, defaulter til true")
+				return@getOrElse true
+			}
 		}
 	}
 
