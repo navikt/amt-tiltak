8f6b00c43d07233c3ba2af4b2a81785279ea16bd
Alexander Gård <alex.gaard@hotmail.com>
2022-05-31 11:28:34 +0200

    La til tilganger for tiltaksansvarlig /meg endepunkt

--- tilgangskontroll/src/main/kotlin/no/nav/amt/tiltak/tilgangskontroll/ad_gruppe/AdGruppeService.kt
+++ tilgangskontroll/src/main/kotlin/no/nav/amt/tiltak/tilgangskontroll/ad_gruppe/AdGruppeService.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 import java.time.Duration
 
 @Service
-class AdGruppeService(
+open class AdGruppeService(
 	private val poaoTilgangClient: PoaoTilgangClient
 )  {
 
@@ -17,13 +17,13 @@ class AdGruppeService(
 		.maximumSize(10_000)
 		.build<String, List<AdGruppe>>()
 
-	fun hentAdGrupper(navIdent: String): List<AdGruppe> {
+	open fun hentAdGrupper(navIdent: String): List<AdGruppe> {
 		return tryCacheFirstNotNull(navIdentToAdGruppeCache, navIdent) {
 			poaoTilgangClient.hentAdGrupper(navIdent)
 		}
 	}
 
-	fun erMedlemAvGruppe(navIdent: String, adGruppeNavn: String): Boolean {
+	open fun erMedlemAvGruppe(navIdent: String, adGruppeNavn: String): Boolean {
 		return hentAdGrupper(navIdent).any { it.name == adGruppeNavn }
 	}
 
