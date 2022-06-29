a18917e7c8daa21e467c032f57ec28072bc3b050
Alexander Gård <alex.gaard@hotmail.com>
2022-06-29 08:15:09 +0200

    Flytt ingestors til kafka-modul

---
+++ kafka/kafka-config/src/main/kotlin/no/nav/amt/tiltak/kafka/config/KafkaConfiguration.kt
@@ -0,0 +1,122 @@
+package no.nav.amt.tiltak.kafka.config
+
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
+import no.nav.amt.tiltak.core.kafka.ArenaAclIngestor
+import no.nav.amt.tiltak.core.kafka.EndringPaaBrukerIngestor
+import no.nav.amt.tiltak.core.kafka.NavEnhetIngestor
+import no.nav.amt.tiltak.core.kafka.TildeltVeilederIngestor
+import no.nav.common.kafka.consumer.KafkaConsumerClient
+import no.nav.common.kafka.consumer.feilhandtering.KafkaConsumerRecordProcessor
+import no.nav.common.kafka.consumer.feilhandtering.util.KafkaConsumerRecordProcessorBuilder
+import no.nav.common.kafka.consumer.util.KafkaConsumerClientBuilder
+import no.nav.common.kafka.consumer.util.deserializer.Deserializers.stringDeserializer
+import no.nav.common.kafka.producer.KafkaProducerClient
+import no.nav.common.kafka.producer.KafkaProducerClientImpl
+import no.nav.common.kafka.spring.PostgresJdbcTemplateConsumerRepository
+import okhttp3.internal.toImmutableList
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.jdbc.core.JdbcTemplate
+import java.util.function.Consumer
+
+
+@Configuration
+@EnableConfigurationProperties(KafkaTopicProperties::class)
+open class KafkaConfiguration(
+	kafkaTopicProperties: KafkaTopicProperties,
+	kafkaProperties: KafkaProperties,
+	jdbcTemplate: JdbcTemplate,
+	arenaAclIngestor: ArenaAclIngestor,
+	tildeltVeilederIngestor: TildeltVeilederIngestor,
+	endringPaaBrukerIngestor: EndringPaaBrukerIngestor,
+	navEnhetIngestor: NavEnhetIngestor
+) {
+	private val log = LoggerFactory.getLogger(javaClass)
+    private var client: KafkaConsumerClient
+	private var consumerRepository = PostgresJdbcTemplateConsumerRepository(jdbcTemplate)
+	private var consumerRecordProcessor: KafkaConsumerRecordProcessor
+
+	init {
+		val topicConfigs = mutableListOf<KafkaConsumerClientBuilder.TopicConfig<String, String>>()
+
+		topicConfigs.addAll(
+			listOf(
+				KafkaConsumerClientBuilder.TopicConfig<String, String>()
+					.withLogging()
+					.withStoreOnFailure(consumerRepository)
+					.withConsumerConfig(
+						kafkaTopicProperties.amtTiltakTopic,
+						stringDeserializer(),
+						stringDeserializer(),
+						Consumer<ConsumerRecord<String, String>> { arenaAclIngestor.ingestKafkaRecord(it.value()) }
+					)
+			)
+		)
+
+		topicConfigs.add(
+			KafkaConsumerClientBuilder.TopicConfig<String, String>()
+				.withLogging()
+				.withStoreOnFailure(consumerRepository)
+				.withConsumerConfig(
+					kafkaTopicProperties.sisteTilordnetVeilederTopic,
+					stringDeserializer(),
+					stringDeserializer(),
+					Consumer<ConsumerRecord<String, String>> { tildeltVeilederIngestor.ingestKafkaRecord(it.value()) }
+				)
+		)
+
+		topicConfigs.add(
+			KafkaConsumerClientBuilder.TopicConfig<String, String>()
+				.withLogging()
+				.withStoreOnFailure(consumerRepository)
+				.withConsumerConfig(
+					kafkaTopicProperties.endringPaaBrukerTopic,
+					stringDeserializer(),
+					stringDeserializer(),
+					Consumer<ConsumerRecord<String, String>> { endringPaaBrukerIngestor.ingestKafkaRecord(it.value()) }
+				)
+		)
+
+		topicConfigs.add(
+			KafkaConsumerClientBuilder.TopicConfig<String, String>()
+				.withLogging()
+				.withStoreOnFailure(consumerRepository)
+				.withConsumerConfig(
+					kafkaTopicProperties.navEnhetTopic,
+					stringDeserializer(),
+					stringDeserializer(),
+					Consumer<ConsumerRecord<String, String>> { navEnhetIngestor.ingestKafkaRecord(it.value()) }
+				)
+		)
+
+		consumerRecordProcessor = KafkaConsumerRecordProcessorBuilder
+			.builder()
+			.withLockProvider(JdbcTemplateLockProvider(jdbcTemplate))
+			.withKafkaConsumerRepository(consumerRepository)
+			.withConsumerConfigs(topicConfigs.map { it.consumerConfig })
+			.build()
+
+		client = KafkaConsumerClientBuilder.builder()
+            .withProperties(kafkaProperties.consumer())
+            .withTopicConfigs(topicConfigs.toImmutableList())
+            .build()
+    }
+
+	@Bean
+	open fun kafkaProducer(kafkaProperties: KafkaProperties): KafkaProducerClient<String, String> {
+		return KafkaProducerClientImpl(kafkaProperties.producer())
+	}
+
+	@EventListener
+	open fun onApplicationEvent(_event: ContextRefreshedEvent?) {
+		log.info("Starting kafka consumer and stored record processor...")
+		client.start()
+		consumerRecordProcessor.start()
+	}
+
+}
---
+++ kafka/kafka-config/src/main/kotlin/no/nav/amt/tiltak/kafka/config/KafkaTopicProperties.kt
@@ -0,0 +1,11 @@
+package no.nav.amt.tiltak.kafka.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "app.env")
+data class KafkaTopicProperties(
+	var amtTiltakTopic: String = "",
+	var sisteTilordnetVeilederTopic: String = "",
+	var endringPaaBrukerTopic: String = "",
+	var navEnhetTopic: String = ""
+)
