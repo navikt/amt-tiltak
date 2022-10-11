422c8f667580d9a59581bfc07c68c442a81b18c3
Tormod Fossum <tormod.fossum@nav.no>
2022-10-11 15:26:58 +0200

    Send aktive endringsmeldinger med sluttdato til deltakeroversikten

--- bff-tiltaksarrangor/src/main/kotlin/no/nav/amt/tiltak/bff/tiltaksarrangor/dto/AktivEndringsmeldingDto.kt
+++ bff-tiltaksarrangor/src/main/kotlin/no/nav/amt/tiltak/bff/tiltaksarrangor/dto/AktivEndringsmeldingDto.kt
@@ -4,9 +4,11 @@ import no.nav.amt.tiltak.core.domain.tiltak.Endringsmelding
 import java.time.LocalDate
 
 data class AktivEndringsmeldingDto(
-	val startDato: LocalDate?
+	val startDato: LocalDate?,
+	val sluttDato: LocalDate?,
 )
 
 fun Endringsmelding.toDto() = AktivEndringsmeldingDto(
-	startDato = startDato
+	startDato = startDato,
+	sluttDato = sluttDato,
 )
