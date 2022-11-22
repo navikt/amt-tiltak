e43a97c634c714f65545d6a21b59fe9966b9ac19
Tormod Fossum <tormod.fossum@nav.no>
2022-11-22 09:08:47 +0100

    Fjern ubrukte felter

--- bff-tiltaksarrangor/src/main/kotlin/no/nav/amt/tiltak/bff/tiltaksarrangor/dto/PersonDto.kt
+++ bff-tiltaksarrangor/src/main/kotlin/no/nav/amt/tiltak/bff/tiltaksarrangor/dto/PersonDto.kt
@@ -1,14 +1,11 @@
 package no.nav.amt.tiltak.bff.tiltaksarrangor.dto
 
-import no.nav.amt.tiltak.core.port.Diskresjonskode
 import no.nav.amt.tiltak.core.port.Person
 
 data class PersonDto(
 	val fornavn: String,
 	val mellomnavn: String?,
 	val etternavn: String,
-	val telefonnummer: String?,
-	val diskresjonskode: Diskresjonskode?
 )
 
 fun Person.toDto(): PersonDto {
@@ -16,7 +13,5 @@ fun Person.toDto(): PersonDto {
 		fornavn = fornavn,
 		mellomnavn = mellomnavn,
 		etternavn = etternavn,
-		telefonnummer = telefonnummer,
-		diskresjonskode = diskresjonskode,
 	)
 }
