43871a6db83f6984a7126c0811c62f05cf52ed7e
Izis Frøyhov <izis.froyhov@nav.no>
2022-12-22 11:52:19 +0100

    Ansatte uten tilgang kan ikke arkivere endringsmeldinger på skjermede deltakere

--- bff-nav_ansatt/src/main/kotlin/no/nav/amt/tiltak/bff/nav_ansatt/EndringsmeldingController.kt
+++ bff-nav_ansatt/src/main/kotlin/no/nav/amt/tiltak/bff/nav_ansatt/EndringsmeldingController.kt
@@ -3,10 +3,12 @@ package no.nav.amt.tiltak.bff.nav_ansatt
 import no.nav.amt.tiltak.bff.nav_ansatt.dto.EndringsmeldingDto
 import no.nav.amt.tiltak.common.auth.AuthService
 import no.nav.amt.tiltak.common.auth.Issuer
+import no.nav.amt.tiltak.core.exceptions.UnauthorizedException
 import no.nav.amt.tiltak.core.port.DeltakerService
 import no.nav.amt.tiltak.core.port.EndringsmeldingService
 import no.nav.amt.tiltak.core.port.NavAnsattService
 import no.nav.amt.tiltak.core.port.TiltaksansvarligAutoriseringService
+import no.nav.amt.tiltak.log.SecureLog.secureLog
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -40,7 +42,7 @@ class EndringsmeldingController(
 	fun markerFerdig(@PathVariable("endringsmeldingId") endringsmeldingId: UUID) {
 		val navAnsattAzureId = authService.hentAzureIdTilInnloggetBruker()
 		val navIdent = authService.hentNavIdentTilInnloggetBruker()
-
+		val tilgangTilSkjermede = authService.harTilgangTilSkjermedePersoner()
 		val endringsmelding = endringsmeldingService.hentEndringsmelding(endringsmeldingId)
 
 		val deltaker = deltakerService.hentDeltaker(endringsmelding.deltakerId)
@@ -48,8 +50,12 @@ class EndringsmeldingController(
 
 		tiltaksansvarligAuthService.verifiserTilgangTilEndringsmelding(navAnsattAzureId)
 		tiltaksansvarligAuthService.verifiserTilgangTilGjennomforing(navIdent, deltaker.gjennomforingId)
-
 		val navAnsatt = navAnsattService.getNavAnsatt(navIdent)
+
+		if (!tilgangTilSkjermede && deltaker.erSkjermet) {
+			secureLog.error("nav ansatt: <${navAnsatt}> kan ikke arkivere endringsmelding på skjermet person")
+			throw UnauthorizedException("Har ikke tilgang til operasjon på skjermet person. Sjekk secure logs")
+		}
 
 		endringsmeldingService.markerSomUtfort(endringsmeldingId, navAnsatt.id)
 	}
