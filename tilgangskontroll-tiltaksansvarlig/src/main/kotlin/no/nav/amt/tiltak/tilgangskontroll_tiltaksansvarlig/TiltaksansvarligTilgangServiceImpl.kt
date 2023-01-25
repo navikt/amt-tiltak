585f1b8e015fd0206ccf5dd16ffd0670deae5185
Magnus <magnus.evensberget@nav.no>
2023-01-25 10:33:00 +0100

    Ekstra logging (#349)
    * Fjernet whitelist
    
    * Ekstra logging for å finne ut når brukere trykker "legg til" flere ganger på samme gjennomføring
    
    * Testfiks

--- tilgangskontroll-tiltaksansvarlig/src/main/kotlin/no/nav/amt/tiltak/tilgangskontroll_tiltaksansvarlig/TiltaksansvarligTilgangServiceImpl.kt
+++ tilgangskontroll-tiltaksansvarlig/src/main/kotlin/no/nav/amt/tiltak/tilgangskontroll_tiltaksansvarlig/TiltaksansvarligTilgangServiceImpl.kt
@@ -1,8 +1,10 @@
 package no.nav.amt.tiltak.tilgangskontroll_tiltaksansvarlig
 
 import no.nav.amt.tiltak.core.domain.tilgangskontroll.TiltaksansvarligGjennomforingTilgang
+import no.nav.amt.tiltak.core.exceptions.HarAlleredeTilgangException
 import no.nav.amt.tiltak.core.port.NavAnsattService
 import no.nav.amt.tiltak.core.port.TiltaksansvarligTilgangService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
@@ -11,9 +13,11 @@ import java.util.*
 
 @Service
 open class TiltaksansvarligTilgangServiceImpl(
-    private val navAnsattService: NavAnsattService,
-    private val tiltaksansvarligGjennomforingTilgangRepository: TiltaksansvarligGjennomforingTilgangRepository
+	private val navAnsattService: NavAnsattService,
+	private val tiltaksansvarligGjennomforingTilgangRepository: TiltaksansvarligGjennomforingTilgangRepository
 ) : TiltaksansvarligTilgangService {
+
+	private val log = LoggerFactory.getLogger(javaClass)
 
 	private val defaultGyldigTil = ZonedDateTime.parse("3000-01-01T00:00:00.00000+00:00")
 
@@ -30,8 +34,8 @@ open class TiltaksansvarligTilgangServiceImpl(
 		val tilganger = hentAktiveTilganger(navAnsattId)
 
 		if (tilganger.any { it.gjennomforingId == gjennomforingId }) {
-			// Kast heller custom exception og map
-			throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Har allerede tilgang til gjennomføring")
+			log.warn("Nav ansatt med id $navAnsattId prøver å gi tilgang til gjennomføring med id $gjennomforingId, men har det allerede.")
+			throw HarAlleredeTilgangException("Har allerede tilgang til gjennomføring")
 		}
 
 		tiltaksansvarligGjennomforingTilgangRepository.opprettTilgang(
