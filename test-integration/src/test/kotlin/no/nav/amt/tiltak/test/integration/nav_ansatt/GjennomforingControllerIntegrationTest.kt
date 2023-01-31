d791ef2f138c9634deab90edba7280e83f780639
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2023-01-31 11:27:51 +0100

    Legg til status og datoer i HentGjennomforingMedLopenrDto (#356)

--- test-integration/src/test/kotlin/no/nav/amt/tiltak/test/integration/nav_ansatt/GjennomforingControllerIntegrationTest.kt
+++ test-integration/src/test/kotlin/no/nav/amt/tiltak/test/integration/nav_ansatt/GjennomforingControllerIntegrationTest.kt
@@ -130,7 +130,9 @@ class GjennomforingControllerIntegrationTest : IntegrationTestBase() {
 			headers = mapOf("Authorization" to "Bearer $token")
 		)
 
-		val expectedJson = """[{"id":"b3420940-5479-48c8-b2fa-3751c7a33aa2","navn":"Tiltaksgjennomforing1","lopenr":123,"opprettetAr":2020,"arrangorNavn":"Org Tiltaksarrangør 1","tiltak":{"kode":"AMO","navn":"Tiltak1"}}]"""
+		val expectedJson = """
+			[{"id":"b3420940-5479-48c8-b2fa-3751c7a33aa2","navn":"Tiltaksgjennomforing1","lopenr":123,"status":"GJENNOMFORES","startDato":"2022-02-01","sluttDato":"2050-12-30","opprettetAr":2020,"arrangorNavn":"Org Tiltaksarrangør 1","tiltak":{"kode":"AMO","navn":"Tiltak1"}}]
+		""".trimIndent()
 
 		response.code shouldBe 200
 		response.body?.string() shouldBe expectedJson
