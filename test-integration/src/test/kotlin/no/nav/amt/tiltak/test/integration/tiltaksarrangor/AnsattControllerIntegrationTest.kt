9662952913f504028e3a048ef9b9fb98eb424cb9
Magnus Evensberget <magnus.evensberget@nav.no>
2023-02-03 14:06:02 +0100

    /me endepunktet returnerer nå alltid 200, og kun arrangører man er KOORDINATOR på

--- test-integration/src/test/kotlin/no/nav/amt/tiltak/test/integration/tiltaksarrangor/AnsattControllerIntegrationTest.kt
+++ test-integration/src/test/kotlin/no/nav/amt/tiltak/test/integration/tiltaksarrangor/AnsattControllerIntegrationTest.kt
@@ -2,7 +2,10 @@ package no.nav.amt.tiltak.test.integration.tiltaksarrangor
 
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import no.nav.amt.tiltak.ansatt.AnsattDto
 import no.nav.amt.tiltak.ansatt.ArrangorAnsattRepository
+import no.nav.amt.tiltak.common.json.JsonUtils
+import no.nav.amt.tiltak.core.domain.tilgangskontroll.ArrangorAnsattRolle
 import no.nav.amt.tiltak.test.database.DbTestDataUtils
 import no.nav.amt.tiltak.test.database.data.TestData
 import no.nav.amt.tiltak.test.database.data.TestData.ARRANGOR_1
@@ -56,14 +59,33 @@ class AnsattControllerIntegrationTest : IntegrationTestBase() {
 	}
 
 	@Test
-	fun `getInnloggetAnsatt() should return 403 when not only veileder`() {
+	fun `getInnloggetAnsatt() should return 200 and no arrangorer when only VEILEDER`() {
 		val response = sendRequest(
 			method = "GET",
 			url = "/api/tiltaksarrangor/ansatt/meg",
 			headers = mapOf("Authorization" to "Bearer ${mockOAuthServer.issueTokenXToken(TestData.ARRANGOR_ANSATT_2.personligIdent)}")
 		)
 
-		response.code shouldBe 403
+		response.code shouldBe 200
+		val responseBody: AnsattDto? = response.body?.string()?.let { JsonUtils.fromJsonString(it) }
+
+		responseBody shouldNotBe null
+		responseBody!!.arrangorer.isEmpty() shouldBe true
+	}
+
+	@Test
+	fun `getInnloggetAnsatt() should return 200 and only arrangorer with KOORDINATOR roller`() {
+		val response = sendRequest(
+			method = "GET",
+			url = "/api/tiltaksarrangor/ansatt/meg",
+			headers = mapOf("Authorization" to "Bearer ${mockOAuthServer.issueTokenXToken(TestData.ARRANGOR_ANSATT_1.personligIdent)}")
+		)
+
+		response.code shouldBe 200
+		val responseBody: AnsattDto? = response.body?.string()?.let { JsonUtils.fromJsonString(it) }
+
+		responseBody shouldNotBe null
+		responseBody!!.arrangorer.none { it.roller.contains(ArrangorAnsattRolle.VEILEDER.name) } shouldBe true
 	}
 
 	@Test
