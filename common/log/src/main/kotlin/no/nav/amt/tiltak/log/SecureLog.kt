a5526d792298856bd7695bacfe82de53267d16fb
Izis Frøyhov <izis.froyhov@nav.no>
2023-02-07 13:29:03 +0100

    fikser secure logs
    Co-authored-by: Magnus Evensberget <magnus.evensberget@nav.no>

--- common/log/src/main/kotlin/no/nav/amt/tiltak/log/SecureLog.kt
+++ common/log/src/main/kotlin/no/nav/amt/tiltak/log/SecureLog.kt
@@ -3,5 +3,5 @@ package no.nav.amt.tiltak.log
 import org.slf4j.LoggerFactory
 
 object SecureLog {
-	val secureLog = LoggerFactory.getLogger("secureLog")
+	val secureLog = LoggerFactory.getLogger("SecureLog")
 }
