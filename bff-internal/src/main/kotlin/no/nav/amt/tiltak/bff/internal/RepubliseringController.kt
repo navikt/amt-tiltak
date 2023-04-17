8b7d7534c41d439d5d7f061b618a26b4db6a086e
Magnus Evensberget <magnus.evensberget@nav.no>
2023-04-17 17:42:34 +0200

    Merge konflikt

--- bff-internal/src/main/kotlin/no/nav/amt/tiltak/bff/internal/RepubliseringController.kt
+++ bff-internal/src/main/kotlin/no/nav/amt/tiltak/bff/internal/RepubliseringController.kt
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import java.util.*
-import javax.servlet.http.HttpServletRequest
 
 @Unprotected
 @RestController
