871b71af7e7aa7da177f7e7c77b554c7cbdd17f0
Izis Frøyhov <izis.froyhov@nav.no>
2023-05-30 12:45:30 +0200

    Whitelister ny pilotgjennomføring (#485)

--- bff-tiltaksarrangor/src/main/kotlin/no/nav/amt/tiltak/bff/tiltaksarrangor/GjennomforingController.kt
+++ bff-tiltaksarrangor/src/main/kotlin/no/nav/amt/tiltak/bff/tiltaksarrangor/GjennomforingController.kt
@@ -184,6 +184,6 @@ class GjennomforingController(
 	}
 
 	private fun erPilot(gjennomforingId: UUID): Boolean {
-		return gjennomforingId.toString() in listOf("69afc1b8-50b9-472a-8b92-254dec821c3a", "e41ef5c5-2c2e-41f6-97a2-36fca4902b86")
+		return gjennomforingId.toString() in listOf("69afc1b8-50b9-472a-8b92-254dec821c3a", "e41ef5c5-2c2e-41f6-97a2-36fca4902b86", "67b63927-3c6f-494b-ad9b-5fff08b8d196")
 	}
 }
