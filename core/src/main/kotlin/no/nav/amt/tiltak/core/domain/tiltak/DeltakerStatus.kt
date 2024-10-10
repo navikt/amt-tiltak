7a7cfb97b6bebf56e8a63a2c798e20a465caff1c
tiafiring <tiafiring@gmail.com>
2024-10-10 14:06:44 +0200

    legger til status- og statusårsakstekst etter ønske fra valp, oppdate… (#864)
    * legger til status- og statusårsakstekst etter ønske fra valp, oppdaterer dokumentasjon
    
    * fikser readme

--- core/src/main/kotlin/no/nav/amt/tiltak/core/domain/tiltak/DeltakerStatus.kt
+++ core/src/main/kotlin/no/nav/amt/tiltak/core/domain/tiltak/DeltakerStatus.kt
@@ -48,3 +48,38 @@ fun DeltakerStatus.harIkkeStartet() : Boolean {
 	return type in HAR_IKKE_STARTET_STATUSER
 }
 
+fun DeltakerStatus.Type.getStatustekst(): String {
+	return when (this) {
+		DeltakerStatus.Type.UTKAST_TIL_PAMELDING -> "Utkastet er delt og venter på godkjenning"
+		DeltakerStatus.Type.AVBRUTT_UTKAST -> "Avbrutt utkast"
+		DeltakerStatus.Type.VENTER_PA_OPPSTART -> "Venter på oppstart"
+		DeltakerStatus.Type.DELTAR -> "Deltar"
+		DeltakerStatus.Type.HAR_SLUTTET -> "Har sluttet"
+		DeltakerStatus.Type.IKKE_AKTUELL -> "Ikke aktuell"
+		DeltakerStatus.Type.SOKT_INN -> "Søkt om plass"
+		DeltakerStatus.Type.VURDERES -> "Vurderes"
+		DeltakerStatus.Type.VENTELISTE -> "På venteliste"
+		DeltakerStatus.Type.AVBRUTT -> "Avbrutt"
+		DeltakerStatus.Type.FULLFORT -> "Fullført"
+		DeltakerStatus.Type.FEILREGISTRERT -> "Feilregistrert"
+		DeltakerStatus.Type.PABEGYNT_REGISTRERING -> "Påbegynt registrering"
+		DeltakerStatus.Type.PABEGYNT -> "Påbegynt registrering"
+	}
+}
+
+fun DeltakerStatus.Aarsak.getVisningsnavn(beskrivelse: String?): String {
+	if (beskrivelse != null) {
+		return beskrivelse
+	}
+	return when (this) {
+		DeltakerStatus.Aarsak.SYK -> "Syk"
+		DeltakerStatus.Aarsak.FATT_JOBB -> "Fått jobb"
+		DeltakerStatus.Aarsak.TRENGER_ANNEN_STOTTE -> "Trenger annen støtte"
+		DeltakerStatus.Aarsak.FIKK_IKKE_PLASS -> "Fikk ikke plass"
+		DeltakerStatus.Aarsak.IKKE_MOTT -> "Møter ikke opp"
+		DeltakerStatus.Aarsak.ANNET -> "Annet"
+		DeltakerStatus.Aarsak.AVLYST_KONTRAKT -> "Avlyst kontrakt"
+		DeltakerStatus.Aarsak.UTDANNING -> "Utdanning"
+		DeltakerStatus.Aarsak.SAMARBEIDET_MED_ARRANGOREN_ER_AVBRUTT -> "Samarbeidet med arrangøren er avbrutt"
+	}
+}
--- kafka/kafka-producer/src/main/kotlin/no/nav/amt/tiltak/kafka/producer/dto/DeltakerStatusDto.kt
+++ kafka/kafka-producer/src/main/kotlin/no/nav/amt/tiltak/kafka/producer/dto/DeltakerStatusDto.kt
@@ -1,16 +1,22 @@
 package no.nav.amt.tiltak.kafka.producer.dto
 
 import no.nav.amt.tiltak.core.domain.tiltak.DeltakerStatus
+import no.nav.amt.tiltak.core.domain.tiltak.getStatustekst
+import no.nav.amt.tiltak.core.domain.tiltak.getVisningsnavn
 import java.time.LocalDateTime
 
 data class DeltakerStatusDto(
 	val type: DeltakerStatus.Type,
+	val statusTekst: String,
 	val aarsak: DeltakerStatus.Aarsak?,
+	val aarsakTekst: String?,
 	val opprettetDato: LocalDateTime,
 )
 
 fun DeltakerStatus.toDto() = DeltakerStatusDto(
 	type = type,
+	statusTekst = type.getStatustekst(),
 	aarsak = aarsak,
+	aarsakTekst = aarsak?.getVisningsnavn(aarsaksbeskrivelse),
 	opprettetDato = opprettetDato
 )
