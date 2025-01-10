7071bd2775c300911577ad284a6562d448a53b3b
Tia Firing <tia.firing@nav.no>
2025-01-10 08:37:59 +0100

    må legge til typeinfo så bff kan lese meldingene

--- core/src/main/kotlin/no/nav/amt/tiltak/core/domain/tiltak/Vurdering.kt
+++ core/src/main/kotlin/no/nav/amt/tiltak/core/domain/tiltak/Vurdering.kt
@@ -1,8 +1,10 @@
 package no.nav.amt.tiltak.core.domain.tiltak
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.time.LocalDateTime
 import java.util.UUID
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 data class Vurdering(
 	val id: UUID,
 	val deltakerId: UUID,
