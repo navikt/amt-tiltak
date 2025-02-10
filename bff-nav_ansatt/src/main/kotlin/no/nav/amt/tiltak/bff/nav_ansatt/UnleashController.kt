6b2c82e68504a80f5e3a890a79970ffb254d38c8
Izis Frøyhov <izis.froyhov@nav.no>
2025-02-10 08:54:56 +0100

    Gjøre det mulig å hente toggles i tiltaksansvarlig flate

--- bff-nav_ansatt/src/main/kotlin/no/nav/amt/tiltak/bff/nav_ansatt/UnleashController.kt
+++ bff-nav_ansatt/src/main/kotlin/no/nav/amt/tiltak/bff/nav_ansatt/UnleashController.kt
@@ -4,18 +4,21 @@ import no.nav.amt.tiltak.common.auth.Issuer
 import no.nav.amt.tiltak.core.port.UnleashService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("UnleashControllerNavAnsatt")
+@RequestMapping("/api/nav-ansatt/unleash")
+
 open class UnleashController(
     private val unleash: UnleashService
 ) {
 
+	@GetMapping("/feature")
 	@ProtectedWithClaims(issuer = Issuer.AZURE_AD)
-	@GetMapping("/api/nav-ansatt/unleash/feature")
 	fun getToggles(
-		@RequestParam(name = "features") features: List<String>
+		@RequestParam("features") features: List<String>
 	): Map<String, Boolean> {
 		val toggles = getFeaturetoggles(features)
 		return toggles
