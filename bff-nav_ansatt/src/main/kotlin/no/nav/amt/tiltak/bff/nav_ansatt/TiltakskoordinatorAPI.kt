02b2b68887a2309a8acb2c34f419f23f9a8b138b
Tormod Fossum <tormod.fossum@nav.no>
2025-03-13 10:23:53 +0100

    Fiks dto

--- bff-nav_ansatt/src/main/kotlin/no/nav/amt/tiltak/bff/nav_ansatt/TiltakskoordinatorAPI.kt
+++ bff-nav_ansatt/src/main/kotlin/no/nav/amt/tiltak/bff/nav_ansatt/TiltakskoordinatorAPI.kt
@@ -1,5 +1,6 @@
 package no.nav.amt.tiltak.bff.nav_ansatt
 
+import no.nav.amt.lib.models.deltaker.DeltakerStatus.Aarsak
 import no.nav.amt.tiltak.common.auth.AuthService
 import no.nav.amt.tiltak.common.auth.Issuer
 import no.nav.amt.tiltak.core.domain.tiltak.DeltakerStatus
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 import java.util.UUID
 
 @RestController
@@ -24,9 +26,32 @@ class TiltakskoordinatorAPI(
 	@PostMapping
 	fun delMedArrangor(
 		@RequestBody body: List<UUID>,
-    ): Map<UUID, DeltakerStatus> {
+    ): Map<UUID, DeltMedArrangorStatus> {
 		authService.validerErM2MToken()
 
-		return deltakerService.delMedArrangor(body)
+		return deltakerService.delMedArrangor(body).mapValues { (_, status) -> status.toDeltMedArrangorStatus()  }
 	}
 }
+
+private fun DeltakerStatus.toDeltMedArrangorStatus(): DeltMedArrangorStatus {
+	require(type == DeltakerStatus.Type.SOKT_INN && aktiv)
+
+	return DeltMedArrangorStatus(
+		id = id,
+		type = type,
+		gyldigFra = gyldigFra,
+		gyldigTil = null,
+		opprettet = opprettetDato,
+		erManueltDeltMedArrangor = erManueltDeltMedArrangor,
+	)
+}
+
+data class DeltMedArrangorStatus(
+	val id: UUID,
+	val type: DeltakerStatus.Type,
+	val aarsak: Aarsak? = null,
+	val gyldigFra: LocalDateTime,
+	val gyldigTil: LocalDateTime?,
+	val opprettet: LocalDateTime,
+	val erManueltDeltMedArrangor: Boolean = false,
+)
